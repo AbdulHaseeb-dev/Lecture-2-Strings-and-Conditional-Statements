d6d738884a8b47d7c2bf839d7d34d9fa489c0396
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-08-24 22:13:14 +0500

    Initial commit

---
+++ Lecture2.py
@@ -0,0 +1,96 @@
+# str1 = "apna"
+# len1 = len(str1)
+# print (len1)
+
+# str2 = "college"
+# len2 = len(str2)
+# print (len2)
+
+# final_str = str1 + " " + str2
+# len3 = len(final_str)
+# print(final_str)
+# print (len3)
+
+# print (final_str[5])
+# print (final_str[5 : 12])
+# print (final_str[5 : len3])
+
+# str = "I am studying python from apna collge"
+# print (str.endswith("ge"))
+# print (str.capitalize())
+# print (str.replace("python", "javascript"))
+# print (str.find("o"))
+# print (str.count("am"))
+
+# WAP to input user’s first name & print its length.
+
+# name = input ("Enter your name : ")
+# print (len(name))
+
+# WAP to find the occurrence of ‘$’ in a String.
+
+# dollar = "hamna works at an it company in the us and gets $12000 monthly compensation"
+# print(dollar.count("$"))
+
+# light = input ("Enter traffic signal :")
+
+# if(light == "green"):
+#     print("u can go")
+# elif(light == "yellow "):
+#     print("stop")
+# elif(light == "red"):
+#     print("stop")
+# else:
+#     print("program stopped")            
+
+
+# marks = int(input("Enter your marks :"))
+
+# if(marks >= 90):
+#     grade = "A"
+# elif(marks >= 80 and marks < 90):
+#     grade = "B"
+# elif(marks >= 70 and marks < 80):
+#     grade = "C" 
+# elif(marks >= 60 and marks < 70):
+#     grade = "D"       
+# else:
+#     print("you fail")
+
+# print("your grade is ",grade)        
+
+# WAP to check if a number entered by the user is odd or even.
+
+# number = int(input("Enter number :"))
+
+# rem = number % 2 
+
+# if(rem == 0):
+#     print("Number is even")
+# else:
+#     print("Number is not even")    
+
+# WAP to find the greatest of 4 numbers entered by the user.
+
+# number1 = int(input("Enter number :"))
+# number2 = int(input("Enter number :"))
+# number3 = int(input("Enter number :"))
+# number4 = int(input("Enter number :"))
+
+# if(number1 > number2 and number1 > number3 and number1 > number4):
+#     print(number1,"is the greatest")
+# elif(number2 > number3 and number2 > number4):
+#     print(number2,"is the greatest") 
+# elif(number3 > number4):
+#     print(number3,"is the greatest")  
+# else:
+#     print(number4,"is the greatest")     
+
+# WAP to check if a number is a multiple of 7 or not
+
+multiple = int(input("Enter multiple of 7 :"))
+
+if(multiple % 7 == 0):
+    print(multiple,"is a multiple of 7")
+else:
+    print(multiple,"is not a multiple of 7")    
